11ff932f95a7dc3f696111f73e130f641ecdd25d
Anuj Mittal <anuj.mittal@oss.qualcomm.com>
2026-05-25 19:31:40 +0800

    scripts: add license and SPDX identifiers
    Signed-off-by: Anuj Mittal <anuj.mittal@oss.qualcomm.com>

--- scripts/extract_flash_artifacts.sh
+++ scripts/extract_flash_artifacts.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: BSD-3-Clause
 set -euo pipefail
 
 if [[ $# -lt 1 || $# -gt 2 ]]; then
